ffa65e709e8b3702cc829ebfab52a16adf4784cb
Pavel <pryabov@asu.edu>
2024-09-19 16:11:35 -0700

    init commit and push

---
+++ ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
@@ -0,0 +1,36 @@
+package edu.asu.ASUHelloWorldJavaFXMaven;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class HelloFX extends Application {
+
+    public void start(Stage primaryStage) {
+    	System.out.println("ASU Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Pavel Ryabov Hello World Fall 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Pavel says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Pavel says: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
